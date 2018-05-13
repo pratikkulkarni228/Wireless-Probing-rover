88df7f02e97261bed1a1f9fbc6830fc454bd961e
Chaitanya Uttarwar <32759949+chitti-chitti@users.noreply.github.com>
2018-05-13 08:44:16 +0530

    Code cleanup

--- Codes/Servo/Sweep_final.ino
+++ Codes/Servo/Sweep_final.ino
@@ -1,14 +1,4 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
-//#include<Wire.h>
 Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 

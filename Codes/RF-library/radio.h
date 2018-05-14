d919035535edbbd0bb34de5d5b169f1737420d67
swapnil190296 <swapnil190296@gmail.com>
2018-05-14 13:41:37 -0400

    Adding library implementation

--- Codes/RF-library/radio.h
+++ Codes/RF-library/radio.h
@@ -4,4 +4,24 @@
 		 between it's Rx and Tx modules.
 */
 
+#ifndef RADIO_H
+#define RADIO_H
 
+#include <stdlib.h>
+#include <Arduino.h>
+
+extern "C"
+{
+  extern void set_pins(uint8_t pin_rx, uint8_t pin_tx);
+  extern void setup(uint16_t speed);
+  
+  extern void rf_begin();
+  extern void rf_end();
+  
+  extern uint8_t send_data(uint8_t *buffer, uint8_t length);
+  
+  extern uint8_t recv_data(uint8_t *buffer, uint8_t *length);
+  
+}
+
+#endif

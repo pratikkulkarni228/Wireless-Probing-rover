f1bed76f5c43c5c7676be16878364c0303d4a3f8
Chaitanya Uttarwar <32759949+chitti-chitti@users.noreply.github.com>
2018-05-14 10:14:25 +0530

    Cleanup
    comment corrections and removed generated stubs

--- Codes/Servo/Sweep_final.ino
+++ Codes/Servo/Sweep_final.ino
@@ -1,15 +1,5 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
-// twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
 
@@ -20,15 +10,14 @@ void setup()
 
 void loop() 
 {
-  for (pos = 15; pos <= 165; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
+  for (pos = 15; pos <= 165; pos += 1) { // goes from 15 degrees to 165 degrees // in steps of 1 degree per iteration
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
 
   }
-  for (pos = 165; pos >= 15; pos -= 1) { // goes from 180 degrees to 0 degrees
+  for (pos = 165; pos >= 15; pos -= 1) { // goes from 165 degrees to 15 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(30);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 30ms for the servo to reach the position
   }
 }
 
